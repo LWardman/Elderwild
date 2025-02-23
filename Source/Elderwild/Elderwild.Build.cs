74a1f5a1673eb1250e3aa0dc696ccf851d3e7f3a
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-02-23 21:32:25 +0100

    Added ProceduralMeshComponent to public and private dependencies, as it is being used in Grid.h & Grid.cpp and needs this to be included without error.

--- Source/Elderwild/Elderwild.Build.cs
+++ Source/Elderwild/Elderwild.Build.cs
@@ -8,9 +8,9 @@ public class Elderwild : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ProceduralMeshComponent" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "ProceduralMeshComponent" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
