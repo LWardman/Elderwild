9e51c326251a6dcebf6a53051671053453d5d818
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 15:22:49 +0100

    Added functions to find and get the grid, plus a contructor

--- Source/Elderwild/Public/GameModes/DevGameMode.h
+++ Source/Elderwild/Public/GameModes/DevGameMode.h
@@ -4,10 +4,22 @@
 #include "GameFramework/GameModeBase.h"
 #include "DevGameMode.generated.h"
 
+class AGrid;
 
 UCLASS()
 class ELDERWILD_API ADevGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ADevGameMode();
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+	AGrid* Grid;
+
+	AGrid* FindGrid();
+
+	AGrid* GetGrid();
 };
