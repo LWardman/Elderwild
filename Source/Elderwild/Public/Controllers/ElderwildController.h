509afa72e19975eb0a729db707e6a5e8e16175ab
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 15:22:00 +0100

    Using this controller class to handle input from the player, should include things like translation, zooming and rotating in the future.

--- Source/Elderwild/Public/Controllers/ElderwildController.h
+++ Source/Elderwild/Public/Controllers/ElderwildController.h
@@ -4,10 +4,28 @@
 #include "GameFramework/PlayerController.h"
 #include "ElderwildController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class ELDERWILD_API AElderwildController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AElderwildController();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* ClickAction;
+
+protected:
+	virtual void SetupInputComponent() override;
 	
+	// To add mapping context
+	virtual void BeginPlay();
+
+	void OnClickStarted();
 };
