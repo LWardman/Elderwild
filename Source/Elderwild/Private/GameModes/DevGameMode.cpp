a373ce57037ab3bb1f831cb3e05056cb8e1a314f
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 15:23:38 +0100

    sets default pawn and controller classes. Finds and gets the grid

--- Source/Elderwild/Private/GameModes/DevGameMode.cpp
+++ Source/Elderwild/Private/GameModes/DevGameMode.cpp
@@ -1,3 +1,56 @@
-
 #include "GameModes/DevGameMode.h"
 
+#include "Kismet/GameplayStatics.h"
+
+#include "Controllers/ElderwildController.h"
+#include "Actors/Grid.h"
+
+ADevGameMode::ADevGameMode()
+{
+	PlayerControllerClass = AElderwildController::StaticClass();
+
+	// set default pawn class to our blueprinted pawn
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Elderwild/Pawns/BP_PlayerPawn.BP_PlayerPawn"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/Elderwild/Controllers/BP_ElderwildController.BP_ElderwildController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
+
+void ADevGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Grid = FindGrid();
+}
+
+AGrid* ADevGameMode::FindGrid()
+{
+	TArray<AActor*> GridActors;
+	UGameplayStatics::GetAllActorsOfClass(this, AGrid::StaticClass(), GridActors);
+
+	if (GridActors.IsValidIndex(0))
+	{
+		return Cast<AGrid>(GridActors[0]);
+	}
+	
+	UE_LOG(LogTemp, Error, TEXT("Cannot find grid, returning nullptr"));
+	return nullptr;
+}
+
+AGrid* ADevGameMode::GetGrid()
+{
+	if (Grid == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Grid is a nullptr"));
+	}
+	
+	return Grid;
+}
