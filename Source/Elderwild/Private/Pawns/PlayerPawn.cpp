74c11b47fbcda911b3348fb2578ec2723c97cc67
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 11:21:25 +0100

    Initial Commit

---
+++ Source/Elderwild/Private/Pawns/PlayerPawn.cpp
@@ -0,0 +1,26 @@
+#include "Pawns/PlayerPawn.h"
+
+APlayerPawn::APlayerPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
