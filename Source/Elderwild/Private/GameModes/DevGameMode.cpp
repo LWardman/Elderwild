ed02cc21aa7ddc1bc64146c82abe542ceedba51b
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 11:41:31 +0100

    Initial Commit

---
+++ Source/Elderwild/Private/GameModes/DevGameMode.cpp
@@ -0,0 +1,3 @@
+
+#include "GameModes/DevGameMode.h"
+
---
+++ Source/Elderwild/Public/GameModes/DevGameMode.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevGameMode.generated.h"
+
+
+UCLASS()
+class ELDERWILD_API ADevGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
