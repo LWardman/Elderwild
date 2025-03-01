075e029273ca301af897cd9dbe08ca1c7adf4f05
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-01 15:22:26 +0100

    Starts with mouse cursor enabled so the player can see where they are clicking. basic shell for player input

--- Source/Elderwild/Private/Controllers/ElderwildController.cpp
+++ Source/Elderwild/Private/Controllers/ElderwildController.cpp
@@ -1,2 +1,43 @@
 #include "Controllers/ElderwildController.h"
 
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "EnhancedInputSubsystems.h"
+
+AElderwildController::AElderwildController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
+
+void AElderwildController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AElderwildController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Add Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// Setup mouse input events
+		EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Started, this, &AElderwildController::OnClickStarted);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void AElderwildController::OnClickStarted()
+{
+	UE_LOG(LogTemp, Log, TEXT("Clicked!"));
+}
