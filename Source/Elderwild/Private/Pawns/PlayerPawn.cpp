0f633d0cefe5853382474fc75efa9882d412f0c6
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-02 20:48:46 +0100

    Added comments, moved contents of tick() into its own function DelegateCursorToGrid()

--- Source/Elderwild/Private/Pawns/PlayerPawn.cpp
+++ Source/Elderwild/Private/Pawns/PlayerPawn.cpp
@@ -2,10 +2,8 @@
 
 #include "Actors/Grid.h"
 #include "Camera/CameraComponent.h"
-#include "GameFramework/GameMode.h"
 #include "GameModes/DevGameMode.h"
 #include "Kismet/GameplayStatics.h"
-#include "Kismet/KismetMathLibrary.h"
 
 APlayerPawn::APlayerPawn()
 {
@@ -18,61 +16,68 @@ APlayerPawn::APlayerPawn()
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
 
-	// Create a camera...
+	// Create a camera, set as root and rotation...
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	RootComponent = Camera;
-	Camera->SetRelativeRotation(FRotator(-60, 0, 0));
+	Camera->SetRelativeRotation(FRotator(-60, 0, 0)); // Pointing downwards at a slant
 }
 
 void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void APlayerPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (APlayerController* PC = Cast<APlayerController>(GetController()))
-	{
-		FHitResult Hit;
-		if (PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit))
-		{
-			FVector HitLocation = Hit.Location;
-			FVector ImpactNormal = Hit.ImpactNormal;
-			FRotator CursorRotation = UKismetMathLibrary::MakeRotationFromAxes(ImpactNormal, FVector::Zero(), FVector::Zero());
-			
-			
-			if (ADevGameMode* GM = Cast<ADevGameMode>(UGameplayStatics::GetGameMode(GetWorld())))
-            {
-	            if (AGrid* Grid = GM->GetGrid())
-	            {
-	            	int32 GridRow;
-	            	int32 GridCol;
-	            	bool IsValid;
-		            Grid->LocationToTile(HitLocation, GridRow, GridCol, IsValid);
-	            	Grid->SetSelectedTile(GridRow, GridCol);
-	            }
-            }
-		}
-		else // If nothing found under cursor
-		{
-			if (ADevGameMode* GM = Cast<ADevGameMode>(UGameplayStatics::GetGameMode(GetWorld())))
-			{
-				if (AGrid* Grid = GM->GetGrid())
-				{
-					// Passing invalid row & tile to force no selection
-					Grid->SetSelectedTile(-1, -1);
-				}
-			}
-		}
-	}
-	
+	DelegateCursorToGrid();
 }
 
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
 
+void APlayerPawn::DelegateCursorToGrid() const
+{
+	// Player controller provides access to the player's cursor.
+	APlayerController* PC = Cast<APlayerController>(GetController());
+
+	if (PC == nullptr)	
+	{
+		UE_LOG(LogTemp, Error, TEXT("Could not get player controller, check if its initialised."));
+		return;
+	}
+
+	// Game mode is providing access to the grid.
+	ADevGameMode* GM = Cast<ADevGameMode>(UGameplayStatics::GetGameMode(GetWorld()));
+	if (GM == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Either GameMode does not exist, or it can't be cast to the Dev mode"));
+		return;
+	}
+
+	// Ensure the grid is found and valid.
+	AGrid* Grid = GM->GetGrid();
+	if (Grid == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could not get grid from game mode"));
+		return;
+	}
+	
+	FHitResult Hit;
+	if (PC->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit)) // If successful hit
+	{
+		int32 GridRow;
+		int32 GridCol;
+		bool IsValid;
+		Grid->LocationToTile(Hit.Location, GridRow, GridCol, IsValid);
+		Grid->SetSelectedTile(GridRow, GridCol);
+	}
+	else // If hit isn't successful
+	{
+		// Passing invalid row & tile to force no selection
+		Grid->SetSelectedTile(-1, -1);
+	}
 }
