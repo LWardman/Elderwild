e08d5c1756e838c97d5811c3ec9db4c62e647e00
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-13 19:41:39 +0100

    Added warning about using this class

--- Source/Elderwild/Public/GameModes/DevGameMode.h
+++ Source/Elderwild/Public/GameModes/DevGameMode.h
@@ -6,12 +6,18 @@
 
 class AGrid;
 
+/** WARNING : ADevGameMode should not be used in the final product,
+ *		      it is for testing only. If this game mode wants to be
+ *		      used, then make another class with an identical body so
+ *		      this can be modified for testing new modes.
+ */
 UCLASS()
 class ELDERWILD_API ADevGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
 public:
+	
 	ADevGameMode();
 
 	virtual void BeginPlay() override;
@@ -20,10 +26,9 @@ public:
 
 	void SetDefaultControllerClass();
 
+	// Finds, stores and gets the grid used in play.
 	UPROPERTY()
 	AGrid* Grid;
-
 	AGrid* FindGrid();
-
-	AGrid* GetGrid();
+	AGrid* GetGrid() const;
 };
