9d41909f2882e211ff870ed35d5dbff64c4c8264
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-23 19:02:30 +0100

    Added logic for moving the camera on the XY plane, rotating it around the Z axis and zooming in smoothly

--- Source/Elderwild/Private/Controllers/ElderwildController.cpp
+++ Source/Elderwild/Private/Controllers/ElderwildController.cpp
@@ -3,6 +3,11 @@
 #include "EnhancedInputComponent.h"
 #include "InputActionValue.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraComponent.h"
+
+#include "Pawns/PlayerPawn.h"
 
 AElderwildController::AElderwildController()
 {
@@ -15,6 +20,19 @@ AElderwildController::AElderwildController()
 void AElderwildController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	PlayerPawn = Cast<APlayerPawn>(GetPawn());
+	checkf(PlayerPawn, TEXT("Controller could not find the player pawn"));
+
+	Movement = PlayerPawn->Movement;
+	checkf(Movement, TEXT("Controller could not find the pawns floating movement component"));
+}
+
+void AElderwildController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	InterpolateCameraFieldOfView(DeltaSeconds);
 }
 
 void AElderwildController::SetupInputComponent()
@@ -24,14 +42,24 @@ void AElderwildController::SetupInputComponent()
 	// Add Input Mapping Context
 	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
 	{
-		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		Subsystem->AddMappingContext(IMC_StandardPlay, 0);
 	}
 
 	// Set up action bindings
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
 	{
-		// Setup mouse input events
-		EnhancedInputComponent->BindAction(ClickAction, ETriggerEvent::Started, this, &AElderwildController::OnClickStarted);
+		// Setup LMB click
+		EnhancedInputComponent->BindAction(IA_Click, ETriggerEvent::Started, this, &AElderwildController::OnClickStarted);
+
+		// Setup keyboard camera movement
+		EnhancedInputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AElderwildController::MoveCameraOnXYPlane);
+		EnhancedInputComponent->BindAction(IA_Rotate, ETriggerEvent::Triggered, this, &AElderwildController::RotateCameraAroundYawAxis);
+		EnhancedInputComponent->BindAction(IA_Zoom, ETriggerEvent::Triggered, this, &AElderwildController::ZoomCamera);
+
+		// Setup mouse camera movement
+		EnhancedInputComponent->BindAction(IA_DragMoveCamera, ETriggerEvent::Triggered, this, &AElderwildController::DragMoveCamera);
+
+		EnhancedInputComponent->BindAction(IA_DragRotateCamera, ETriggerEvent::Triggered, this, &AElderwildController::DragRotateCamera);
 	}
 	else
 	{
@@ -42,4 +70,68 @@ void AElderwildController::SetupInputComponent()
 void AElderwildController::OnClickStarted()
 {
 	UE_LOG(LogTemp, Log, TEXT("Clicked!"));
+}
+
+void AElderwildController::MoveCameraOnXYPlane(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	checkf(PlayerPawn, TEXT("Can't rotate without a pawn"));
+	FVector ForwardVector = PlayerPawn->GetActorForwardVector();
+
+	// Quick way to project the forward vector onto the XY plane
+	ForwardVector.Z = 0.0f;			
+	ForwardVector.Normalize();
+	
+	FVector RightVector = PlayerPawn->GetActorRightVector();
+	
+	FVector MovementDirection = ForwardVector * MovementVector.X + RightVector * MovementVector.Y;
+
+	checkf(Movement, TEXT("Cannot move without a movement component"));
+	Movement->AddInputVector(MovementDirection);
+}
+
+void AElderwildController::RotateCameraAroundYawAxis(const FInputActionValue& Value)
+{
+	const float Direction = Value.GetMagnitude();
+
+	checkf(PlayerPawn, TEXT("Can't rotate without a pawn"));
+	FRotator CurrentRotation = PlayerPawn->GetActorRotation();
+
+	float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+	
+	FRotator DeltaRotator = FRotator{0.0f, DeltaTime * RotationSpeed * Direction, 0.0f};
+
+	FRotator NewRotation = CurrentRotation + DeltaRotator;
+	
+	PlayerPawn->SetActorRotation(NewRotation);
+}
+
+void AElderwildController::ZoomCamera(const FInputActionValue& Value)
+{
+	// Increment target FOV by 2 in axis direction
+	TargetFieldOfView -= Value.Get<float>() * 2;
+	TargetFieldOfView = FMath::Clamp(TargetFieldOfView, MinimumFieldOfView, MaximumFieldOfView);
+}
+
+void AElderwildController::DragMoveCamera(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Drag Move Camera!"));
+}
+
+void AElderwildController::DragRotateCamera(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Log, TEXT("Drag Rotate Camera!"));
+}
+
+void AElderwildController::InterpolateCameraFieldOfView(float DeltaSeconds)
+{
+	float DifferenceBetweenTargetAndActualFieldOfView = TargetFieldOfView - FieldOfView;
+	float InterpSpeed = FMath::Abs(DifferenceBetweenTargetAndActualFieldOfView);
+
+	FieldOfView = FMath::FInterpTo(FieldOfView, TargetFieldOfView, DeltaSeconds, InterpSpeed);
+
+	checkf(PlayerPawn, TEXT("Can't zoom without a pawn"));
+	checkf(PlayerPawn->Camera, TEXT("PlayerPawn Camera not found"));
+	PlayerPawn->Camera->SetFieldOfView(FieldOfView);
 }
--- Source/Elderwild/Public/Controllers/ElderwildController.h
+++ Source/Elderwild/Public/Controllers/ElderwildController.h
@@ -4,8 +4,11 @@
 #include "GameFramework/PlayerController.h"
 #include "ElderwildController.generated.h"
 
+class UFloatingPawnMovement;
 class UInputMappingContext;
 class UInputAction;
+class APlayerPawn;
+struct FInputActionValue;
 
 /** Controller intended only for use with the player pawn
  */
@@ -17,12 +20,29 @@ class ELDERWILD_API AElderwildController : public APlayerController
 public:
 	AElderwildController();
 
-	// TODO : configure the mapping context and its InputActions 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
-	UInputMappingContext* DefaultMappingContext;
+	UInputMappingContext* IMC_StandardPlay;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
-	UInputAction* ClickAction;
+	UInputAction* IA_Click;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* IA_Move;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* IA_Rotate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+    UInputAction* IA_Zoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* IA_DragMoveCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* IA_DragRotateCamera;
+
+	UPROPERTY()
+	UFloatingPawnMovement* Movement;
 
 protected:
 	virtual void SetupInputComponent() override;
@@ -30,5 +50,31 @@ protected:
 	// To add mapping context
 	virtual void BeginPlay() override;
 
+	virtual void Tick(float DeltaSeconds) override;
+
 	void OnClickStarted();
+
+	void MoveCameraOnXYPlane(const FInputActionValue& Value);
+
+	void RotateCameraAroundYawAxis(const FInputActionValue& Value);
+
+	void ZoomCamera(const FInputActionValue& Value);
+
+	void DragMoveCamera(const FInputActionValue& Value);
+
+	void DragRotateCamera(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	float RotationSpeed = 10.f;
+
+	float FieldOfView = 90;
+	float TargetFieldOfView = FieldOfView;
+	float MinimumFieldOfView = 16.0f;
+	float MaximumFieldOfView = 90.0f;
+
+	void InterpolateCameraFieldOfView(float DeltaSeconds);
+
+	UPROPERTY()
+	APlayerPawn* PlayerPawn;
 };
