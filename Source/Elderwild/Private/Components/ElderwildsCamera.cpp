b623934a5c972fd4e0cfedcc08cf3f5f88efe9c1
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-28 21:59:03 +0100

    Moved camera logic out of the controller class into here, then the controller calls functions from here. Done because the controller shouldnt be the one doing vector maths.

--- Source/Elderwild/Private/Components/ElderwildsCamera.cpp
+++ Source/Elderwild/Private/Components/ElderwildsCamera.cpp
@@ -1,3 +1,62 @@
-
 #include "Components/ElderwildsCamera.h"
 
+
+UElderwildsCamera::UElderwildsCamera()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UElderwildsCamera::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	InterpolateCameraFieldOfView(DeltaTime);
+}
+
+void UElderwildsCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	SetFieldOfView(FieldOfView);
+}
+
+void UElderwildsCamera::ZoomCamera(float Direction)
+{
+	TargetFieldOfView -= Direction * 2;
+	TargetFieldOfView = FMath::Clamp(TargetFieldOfView, MinimumFieldOfView, MaximumFieldOfView);
+}
+
+void UElderwildsCamera::InterpolateCameraFieldOfView(float DeltaTime)
+{
+	const float DifferenceBetweenTargetAndActualFieldOfView = TargetFieldOfView - FieldOfView;
+	const float InterpSpeed = FMath::Abs(DifferenceBetweenTargetAndActualFieldOfView);
+
+	FieldOfView = FMath::FInterpTo(FieldOfView, TargetFieldOfView, DeltaTime, InterpSpeed);
+
+	SetFieldOfView(FieldOfView);
+}
+
+FVector UElderwildsCamera::CalculateCameraMovementVectorOnXYPlane(FVector2D PlayerInput) const
+{
+	FVector ForwardXY = GetForwardXYVector();
+	FVector RightVector = GetRightVector();
+	
+	return ForwardXY * PlayerInput.X + RightVector * PlayerInput.Y;
+}
+
+FVector UElderwildsCamera::GetForwardXYVector() const 
+{
+	FVector ForwardVector = GetForwardVector();
+
+	FVector ForwardXY = FVector{ ForwardVector.X, ForwardVector.Y, 0.0f };
+	ForwardXY.Normalize();
+
+	return ForwardXY;
+}
+
+void UElderwildsCamera::RotateAroundYawAxis(float RotationMagnitude)
+{
+	FRotator Rotation = GetComponentRotation();
+
+	Rotation.Yaw += RotationMagnitude;
+
+	SetWorldRotation(Rotation);
+}
--- Source/Elderwild/Public/Components/ElderwildsCamera.h
+++ Source/Elderwild/Public/Components/ElderwildsCamera.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,5 +11,28 @@ class ELDERWILD_API UElderwildsCamera : public UCameraComponent
 	GENERATED_BODY()
 
 public:
-	
+	UElderwildsCamera();
+
+protected:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	virtual void BeginPlay() override;
+
+public:
+	void ZoomCamera(float Direction);
+
+private:
+	float FieldOfView = 80;
+	float TargetFieldOfView = FieldOfView;
+	float MinimumFieldOfView = 16.0f;
+	float MaximumFieldOfView = 90.0f;
+
+	void InterpolateCameraFieldOfView(float DeltaTime);
+
+public:
+	FVector CalculateCameraMovementVectorOnXYPlane(FVector2D PlayerInput) const;
+
+	FVector GetForwardXYVector() const;
+
+	void RotateAroundYawAxis(float RotationMagnitude);
 };
