5f23b273383b07af966eaa9e16e968689cfa4e20
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-28 20:44:10 +0100

    Initial Commit

---
+++ Source/Elderwild/Public/Components/ElderwildsCamera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "ElderwildsCamera.generated.h"
+
+
+UCLASS()
+class ELDERWILD_API UElderwildsCamera : public UCameraComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+};
