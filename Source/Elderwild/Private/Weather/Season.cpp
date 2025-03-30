7256d4041c8d0b63a121628439922ecf32aa9b9c
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-30 16:10:51 +0200

    Moved weather entities to their own base class

--- Source/Elderwild/Private/Weather/Season.cpp
+++ Source/Elderwild/Private/Weather/Season.cpp
@@ -1,6 +1,10 @@
 #include "Weather/Season.h"
 
+#include "Weather/WeatherEntity.h"
+
 USeason::USeason()
 {
-	
+	Temperature = CreateDefaultSubobject<UWeatherEntity>("Temperature");
+	Wind = CreateDefaultSubobject<UWeatherEntity>("Wind");
+	Rain = CreateDefaultSubobject<UWeatherEntity>("Rain");
 }
--- Source/Elderwild/Public/Weather/Season.h
+++ Source/Elderwild/Public/Weather/Season.h
@@ -1,8 +1,6 @@
 #pragma once
 
-typedef float Temperature;
-typedef float WindSpeed;
-typedef float Percentage;
+class UWeatherEntity;
 
 UCLASS()
 class ELDERWILD_API USeason : public UObject
@@ -14,12 +12,13 @@ public:
 
 private:
 	int32 NumberOfDays = 20;
-	
-	Temperature TargetTemp = 10.f;
-	WindSpeed TargetWind = 10.f;
-	Percentage TargetRainfall = 10.f;
 
-	Temperature CurrentTemp = TargetTemp;
-	WindSpeed CurrentWind = TargetWind;
-	Percentage CurrentRainfall = TargetRainfall;
+	UPROPERTY()
+	UWeatherEntity* Temperature;
+	
+	UPROPERTY()
+	UWeatherEntity* Wind;
+	
+	UPROPERTY()
+	UWeatherEntity* Rain;
 };
