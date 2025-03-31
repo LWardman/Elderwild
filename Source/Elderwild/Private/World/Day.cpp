d2af14d3e75d64adacf332f12d73e4e47d2cbd59
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-03-31 22:33:32 +0200

    Filled out most logic

--- Source/Elderwild/Private/World/Day.cpp
+++ Source/Elderwild/Private/World/Day.cpp
@@ -1,2 +1,46 @@
 #include "World/Day.h"
 
+UDay::UDay()
+{
+	
+}
+
+void UDay::BeginPlay()
+{
+	BeginDay();
+}
+
+void UDay::BeginDay()
+{
+	GetWorld()->GetTimerManager().SetTimer(
+	DayEndTimerHandle, 
+	this, 
+	&UDay::TransitionToNight, 
+	DaytimeLength, 
+	false);
+
+	DayIsStarting.Broadcast();
+}
+
+void UDay::TransitionToNight()
+{
+	GetWorld()->GetTimerManager().SetTimer(
+	DayEndTimerHandle, 
+	this, 
+	&UDay::EndDay, 
+	NightLength, 
+	false);
+
+	NightIsStarting.Broadcast();
+}
+
+void UDay::EndDay()
+{
+	UE_LOG(LogTemp, Log, TEXT("Day is ending"));
+}
+
+Seconds UDay::GetTimeRemainingForTheDay()
+{
+	const Seconds TimeRemaining = GetWorld()->GetTimerManager().GetTimerRemaining(DayEndTimerHandle);
+	return TimeRemaining;
+}
--- Source/Elderwild/Public/World/Day.h
+++ Source/Elderwild/Public/World/Day.h
@@ -1,14 +1,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "Day.generated.h"
 
+typedef float Seconds;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDayToNightDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FNightToDayDelegate);
+
+
+// TODO : Can a blueprint of this be made and put into the world? How will this work?
 UCLASS()
 class ELDERWILD_API UDay : public UObject
 {
 	GENERATED_BODY()
 
+	UDay();
+
+protected:
+	// TODO : begin play is not valid in a UObject, find an alternative
+	virtual void BeginPlay() override;
+
+private:
+	void BeginDay();
+	void TransitionToNight();
+	void EndDay();
+
+	Seconds DaytimeLength = 60.f;
+	Seconds NightLength = 60.f;
+	Seconds FullDayCycle = DaytimeLength + NightLength;
+
+	FTimerHandle DayEndTimerHandle;
+
+	UPROPERTY(BlueprintAssignable, Category = "Test")
+	FNightToDayDelegate DayIsStarting;
 	
+	UPROPERTY(BlueprintAssignable, Category = "Test")
+	FDayToNightDelegate NightIsStarting;
+
+	Seconds GetTimeRemainingForTheDay();
 };
