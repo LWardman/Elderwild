d866e021535fea9de47e1b46c2bee7a77d93ea18
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-01 22:09:27 +0200

    Added most logic for cycling through days for a season

--- Source/Elderwild/Private/World/Season.cpp
+++ Source/Elderwild/Private/World/Season.cpp
@@ -1,1 +1,36 @@
 #include "World/Season.h"
+
+#include "World/Day.h"
+
+USeason::USeason()
+{
+	Day = CreateDefaultSubobject<UDay>("Day");
+	Day->DayIsStarting.AddDynamic(this, &USeason::OnDayChange);
+}
+
+void USeason::OnDayChange()
+{
+	if (IsLastDayOfMonth())
+	{
+		SeasonIsChanging.Broadcast();
+	}
+}
+
+
+void USeason::SetDayLengths()
+{
+	checkf(Day, TEXT("Day is not initialised"));
+
+	// TODO : pass this information in from a TMap<int32(CurrentDay, Day-Night(struct)>
+	Day->SetNewDayLengths(60.f, 60.f);
+}
+
+int32 USeason::GetNumberOfDaysLeft()
+{
+	return MonthLength - CurrentDay;
+}
+
+bool USeason::IsLastDayOfMonth()
+{
+	return GetNumberOfDaysLeft() == 0;
+}
--- Source/Elderwild/Public/World/Season.h
+++ Source/Elderwild/Public/World/Season.h
@@ -4,11 +4,37 @@
 #include "UObject/Object.h"
 #include "Season.generated.h"
 
-/**
- * 
+class UDay;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSeasonChangeDelegate);
+
+/** A season should...
+ *  - fire events on season start and season end
  */
 UCLASS()
 class ELDERWILD_API USeason : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	USeason();
+
+private:
+	UFUNCTION()
+	void OnDayChange();
+	
+	UPROPERTY()
+	UDay* Day;
+
+	int32 CurrentDay = 1;
+	int32 MonthLength = 28;
+
+	void SetDayLengths();
+
+	int32 GetNumberOfDaysLeft();
+
+	bool IsLastDayOfMonth();
+	
+	UPROPERTY(BlueprintAssignable, Category = "Day Events")
+	FSeasonChangeDelegate SeasonIsChanging;
 };
