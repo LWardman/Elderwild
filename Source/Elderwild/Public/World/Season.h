e16da00e2b8ab69f6513238a9586ca6fbfb7a2bf
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-01 21:17:14 +0200

    Initial Commit

---
+++ Source/Elderwild/Public/World/Season.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Season.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELDERWILD_API USeason : public UObject
+{
+	GENERATED_BODY()
+};
