74339ed97b53a4e4527e776273f63b1f3f747eb9
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-03 21:10:21 +0200

    Initial commit, testing for both success and failure

---
+++ Source/Elderwild/Private/Tests/CalendarTest.cpp
@@ -0,0 +1,6 @@
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FCalendar, "Elderwild.Calendar.ShouldFail", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FCalendar::RunTest(const FString& Parameters)
+{
+	return true;
+}
