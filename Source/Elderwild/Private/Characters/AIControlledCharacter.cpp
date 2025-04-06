e1ecaf1b6b9a798558028e5ffcf8fa571458bad2
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-06 14:44:14 +0200

    Initial Commit

---
+++ Source/Elderwild/Private/Characters/AIControlledCharacter.cpp
@@ -0,0 +1,27 @@
+#include "Characters/AIControlledCharacter.h"
+
+
+AAIControlledCharacter::AAIControlledCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAIControlledCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAIControlledCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AAIControlledCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
