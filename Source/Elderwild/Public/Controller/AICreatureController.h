c7ff3768b7486168ee027440abbb3e47fbcb2e73
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-06 15:11:42 +0200

    Initial Commit

---
+++ Source/Elderwild/Public/Controller/AICreatureController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AICreatureController.generated.h"
+
+
+UCLASS()
+class ELDERWILD_API AAICreatureController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
