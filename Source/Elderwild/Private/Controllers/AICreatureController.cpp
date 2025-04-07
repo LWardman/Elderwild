1ee2a55f0bedf8be85cb3dd38bebcd0dd06a33d8
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-07 20:28:57 +0200

    Added logic to control a creature through a base class

--- Source/Elderwild/Private/Controllers/AICreatureController.cpp
+++ Source/Elderwild/Private/Controllers/AICreatureController.cpp
@@ -1,2 +1,25 @@
 #include "Controllers/AICreatureController.h"
 
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+
+
+AAICreatureController::AAICreatureController()
+{
+	BehaviorTreeManager = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComponent"));
+	checkf(BehaviorTreeManager, TEXT("BehaviorTreeManager could not be created"));
+
+	bStartAILogicOnPossess = true;
+}
+
+void AAICreatureController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+	
+	if (BehaviorTree && UseBlackboard(BehaviorTree->BlackboardAsset, BlackboardComponent))
+	{
+		BehaviorTreeManager->StartTree(*BehaviorTree);
+	}
+}
--- Source/Elderwild/Public/Controllers/AICreatureController.h
+++ Source/Elderwild/Public/Controllers/AICreatureController.h
@@ -4,10 +4,25 @@
 #include "AIController.h"
 #include "AICreatureController.generated.h"
 
+class UBehaviorTree;
+class UBehaviorTreeComponent;
+class UBlackboardComponent;
 
 UCLASS()
 class ELDERWILD_API AAICreatureController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
+	AAICreatureController();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	UBehaviorTree* BehaviorTree;
+
+	UPROPERTY(Transient)
+	UBehaviorTreeComponent* BehaviorTreeManager;
+
+protected:
+	
+	virtual void OnPossess(APawn* InPawn) override;	
 };
