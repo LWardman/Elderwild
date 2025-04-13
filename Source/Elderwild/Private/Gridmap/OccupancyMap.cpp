b2b27b8bc2c4d05e9b9c50a61fecb7ce9a307aff
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-13 20:55:32 +0200

    Filled out logic. Uses a TMap for storing build related info

--- Source/Elderwild/Private/Gridmap/OccupancyMap.cpp
+++ Source/Elderwild/Private/Gridmap/OccupancyMap.cpp
@@ -1,2 +1,27 @@
 #include "Gridmap/OccupancyMap.h"
 
+
+void UOccupancyMap::Init(int32 _GridWidth, int32 _GridHeight)
+{
+	GridWidth = _GridWidth;
+	GridHeight = _GridHeight;
+	
+	for (int i = 0; i < GridWidth; i++)
+	{
+		for (int j = 0; j < GridHeight; j++)
+		{
+			Map.Add(FIntVector2(i, j), EMPTY);
+		}
+	}
+}
+
+FOccupancyState UOccupancyMap::GetTileOccupancyState(FIntVector2 Coord)
+{
+	if (!Map.Contains(Coord)) return OCCUPIED;
+	return Map[Coord];
+}
+
+void UOccupancyMap::SetTileOccupancyState(FIntVector2 Coord, FOccupancyState State)
+{
+	Map[Coord] = State;
+}
--- Source/Elderwild/Public/Gridmap/OccupancyMap.h
+++ Source/Elderwild/Public/Gridmap/OccupancyMap.h
@@ -4,10 +4,30 @@
 #include "UObject/Object.h"
 #include "OccupancyMap.generated.h"
 
+UENUM(BlueprintType)
+enum FOccupancyState : uint8
+{
+	EMPTY,
+	OCCUPIED
+};
+
+typedef TMap<FIntVector2, FOccupancyState> OccupancyMatrix;
 
 UCLASS()
 class ELDERWILD_API UOccupancyMap : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	void Init(int32 _GridWidth, int32 _GridHeight);
+
+	FOccupancyState GetTileOccupancyState(FIntVector2 Coord);
+
+	void SetTileOccupancyState(FIntVector2 Coord, FOccupancyState State);
 	
+private:
+	int32 GridWidth;
+	int32 GridHeight;
+
+	OccupancyMatrix Map;
 };
