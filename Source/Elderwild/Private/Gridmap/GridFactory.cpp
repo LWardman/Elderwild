98e6abe07573c05108e0ab2ddd01aa6f1c2f0c78
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-17 22:18:05 +0200

    Moved logic for geometry creation over from AGrid

--- Source/Elderwild/Private/Gridmap/GridFactory.cpp
+++ Source/Elderwild/Private/Gridmap/GridFactory.cpp
@@ -1,2 +1,76 @@
 #include "Gridmap/GridFactory.h"
 
+#include "Gridmap/GridRenderData.h"
+#include "Gridmap/Line.h"
+
+FGridRenderData UGridFactory::GenerateGridGeometry()
+{
+	FGridRenderData GridGeometry;
+	CreateParallelHorizontalLines(GridGeometry);
+	CreateParallelVerticalLines(GridGeometry);
+	return GridGeometry;
+}
+
+FGridRenderData UGridFactory::GenerateSelectionSquareGeometry()
+{
+	FLine SelectionSquare;
+	SelectionSquare.Start = FVector(0, TileSize/2, 0);
+	SelectionSquare.End = FVector(TileSize, TileSize/2, 0);
+	FGridRenderData SelectionRenderData;
+	CreateLine(SelectionSquare, TileSize, SelectionRenderData);
+	return SelectionRenderData;
+}
+
+void UGridFactory::CreateParallelHorizontalLines(FGridRenderData& GridRenderData)
+{
+	for (int32 i = 0; i <= NumRows; i++)
+	{
+		const float LineStart = i * TileSize;
+		const float LineEnd = GetGridWidth();
+
+		FLine Line;
+		Line.Start = FVector(LineStart, 0, 0);
+		Line.End = FVector(LineStart, LineEnd, 0);
+		
+		CreateLine(Line, LineThickness, GridRenderData);
+	}
+}
+
+void UGridFactory::CreateParallelVerticalLines(FGridRenderData& GridRenderData)
+{
+	for (int32 i = 0; i <= NumCols; i++)
+	{
+		const float LineStart = i * TileSize;
+		const float LineEnd = GetGridHeight();
+
+		FLine Line;
+		Line.Start = FVector( 0, LineStart,0);
+		Line.End = FVector( LineEnd, LineStart,0);
+		
+		CreateLine(Line, LineThickness, GridRenderData);
+	}
+}
+
+void UGridFactory::CreateLine(const FLine& Line, const float Thickness, FGridRenderData& GridRenderData)
+{
+	const float HalfThickness = Thickness / 2;
+
+	FVector LineVector = Line.End - Line.Start;
+	FVector LineVectorNormalized = LineVector.GetSafeNormal();
+	FVector ThicknessDirection = FVector::CrossProduct(LineVectorNormalized, FVector::UnitZ());
+
+	int32 VerticesLength = GridRenderData.Vertices.Num();
+	
+	TArray<int32> TriangleIndices = {VerticesLength + 2, VerticesLength + 1, VerticesLength + 0,
+									 VerticesLength + 2, VerticesLength + 3, VerticesLength + 1};
+
+	GridRenderData.Triangles.Append(TriangleIndices);
+
+	FVector VertexZero = Line.Start + HalfThickness * ThicknessDirection;
+	FVector VertexOne = VertexZero + LineVector;
+	FVector VertexTwo = VertexZero - Thickness * ThicknessDirection;
+	FVector VertexThree = VertexTwo + LineVector;
+
+	TArray<FVector> CalculatedVertices = {VertexZero, VertexOne, VertexTwo, VertexThree};
+	GridRenderData.Vertices.Append(CalculatedVertices);
+}
--- Source/Elderwild/Public/Gridmap/GridFactory.h
+++ Source/Elderwild/Public/Gridmap/GridFactory.h
@@ -1,14 +1,49 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
+#include "Components/ActorComponent.h"
 #include "GridFactory.generated.h"
 
+struct FGridRenderData;
+struct FLine;
+
 /** Handles logic relating to generating vertices and lines for the grid.
+ *  Note that it does *NOT* create the mesh, but generates the geometry of it.
+ *  The geometry is then passed to AGrid to generate.
  */
 UCLASS()
-class ELDERWILD_API UGridFactory : public UObject
+class ELDERWILD_API UGridFactory : public UActorComponent
 {
 	GENERATED_BODY()
+
+public:
+	FGridRenderData GenerateGridGeometry();
+
+	FGridRenderData GenerateSelectionSquareGeometry();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	int32 NumRows = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	int32 NumCols = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	int32 TileSize = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	int32 LineThickness = 10;
+	
+	void CreateParallelHorizontalLines(FGridRenderData& GridRenderData);
+
+	void CreateParallelVerticalLines(FGridRenderData& GridRenderData);
+
+	// TODO : DRY
+	int32 GetGridWidth() const {return NumCols * TileSize;}
+
+	int32 GetGridHeight() const {return NumRows * TileSize;}
+
+	void CreateLine(const FLine& Line, const float Thickness, FGridRenderData& GridRenderData);
+
 	
 };
