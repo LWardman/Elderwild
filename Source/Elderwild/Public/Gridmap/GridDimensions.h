1e4cfff3ce18c689b50e3f500c81848d9b44087c
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-17 23:10:57 +0200

    Added TODO, began moving related functions over to reduce clutter

--- Source/Elderwild/Public/Gridmap/GridDimensions.h
+++ Source/Elderwild/Public/Gridmap/GridDimensions.h
@@ -2,21 +2,25 @@
 
 #include "CoreMinimal.h"
 #include "GridDimensions.generated.h"
-
+// TODO : move related functions here.
 USTRUCT()
 struct FGridDimensions
 {
 	GENERATED_USTRUCT_BODY()
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 NumRows = 10;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 NumCols = 10;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 TileSize = 10;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Constants", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 LineThickness = 10;
+
+	int32 GetGridWidth() const {return NumCols * TileSize;}
+
+	int32 GetGridHeight() const {return NumRows * TileSize;}
 };
