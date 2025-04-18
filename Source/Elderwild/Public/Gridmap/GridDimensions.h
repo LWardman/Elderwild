720574a760f73e1fde8a5f6da9830f3f3c626a58
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-18 17:09:38 +0200

    Moved to be a UActorComponent so it can get its own world location

--- Source/Elderwild/Public/Gridmap/GridDimensions.h
+++ Source/Elderwild/Public/Gridmap/GridDimensions.h
@@ -1,13 +1,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
 #include "GridDimensions.generated.h"
-// TODO : move related functions here.
-USTRUCT()
-struct FGridDimensions
+
+
+UCLASS()
+class ELDERWILD_API UGridDimensions : public UActorComponent
 {
-	GENERATED_USTRUCT_BODY()
+	GENERATED_BODY()
+
+public:
+	int32 GetGridWidth() const {return NumCols * TileSize;}
+	int32 GetGridHeight() const {return NumRows * TileSize;}
+	int32 GetTileSize() const {return TileSize;}
+	int32 GetNumCols() const {return NumRows;}
+	int32 GetNumRows() const {return NumCols;}
+	int32 GetLineThickness() const {return LineThickness;}
+
+	bool TileIsValid(const FIntVector2 Coord) const;
+	bool TileHasValidDimensions() const;
+
+	FVector2D CenterOfTileToGridLocation(FIntVector2 Coord);
+    
+	FVector2D CornerOfTileToGridLocation(FIntVector2 Coord);
+
+	FIntVector2 LocationToTile(FVector HitLocation) const;
 	
+private:
 	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 NumRows = 10;
 
@@ -15,12 +35,12 @@ struct FGridDimensions
 	int32 NumCols = 10;
 
 	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
-	int32 TileSize = 10;
+	int32 TileSize = 200;
 
 	UPROPERTY(EditAnywhere, Category = "Constants", meta = (AllowPrivateAccess = "true"))
 	int32 LineThickness = 10;
 
-	int32 GetGridWidth() const {return NumCols * TileSize;}
+	FVector2D CenterAdjustment();
 
-	int32 GetGridHeight() const {return NumRows * TileSize;}
+	FIntVector2 LocalLocationToTile(FVector LocalHitLocation) const;
 };
