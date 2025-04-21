2c6a740b3fbaed9bc8a5092edd80feda12884474
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-04-21 13:00:06 +0200

    Initial Commit

---
+++ Source/Elderwild/Public/Player/InputDataConfig.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InputDataConfig.generated.h"
+
+UCLASS()
+class ELDERWILD_API UInputDataConfig : public UDataAsset
+{
+	GENERATED_BODY()
+		
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* Click;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* Move;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* Rotate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* Zoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* DragMoveCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Input")
+	UInputAction* DragRotateCamera;
+};
