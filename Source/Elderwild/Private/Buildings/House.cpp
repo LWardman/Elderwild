f86aa0d3c37a55fd6fcfa97902e52ebf8664a1f5
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-05-01 15:59:31 +0200

    Initial Commit

---
+++ Source/Elderwild/Private/Buildings/House.cpp
@@ -0,0 +1,2 @@
+#include "Buildings/House.h"
+
---
+++ Source/Elderwild/Public/Buildings/House.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Buildings/BuildingBase.h"
+#include "House.generated.h"
+
+
+UCLASS()
+class ELDERWILD_API AHouse : public ABuildingBase
+{
+	GENERATED_BODY()
+	
+};
