b01cb2f5de184a80dc37055ffe426fe761bca665
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-05-04 00:47:59 +0200

    Initial commit

---
+++ Source/Elderwild/Private/Tests/GridmapTest.cpp
@@ -0,0 +1,6 @@
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGridmap, "Elderwild.Gridmap", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool FGridmap::RunTest(const FString& Parameters)
+{
+	return true;
+}
