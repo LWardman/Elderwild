d765d78097ee7dcabacf21a4cf2f25876ae1d234
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-05-12 18:16:49 +0200

    Filled out body

--- Source/Elderwild/Private/Buildings/House.cpp
+++ Source/Elderwild/Private/Buildings/House.cpp
@@ -1,2 +1,44 @@
 #include "Buildings/House.h"
 
+#include "Kismet/GameplayStatics.h"
+
+#include "Creatures/CreatureBase.h"
+
+AHouse::AHouse()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AHouse::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHouse::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GetNumberOfInhabitants() < MaxNumberOfCreatures)
+	{
+		TryFillBuildingWithCreatures();
+	}
+}
+
+void AHouse::TryFillBuildingWithCreatures()
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACreatureBase::StaticClass(), FoundActors);
+
+	for (AActor* Actor : FoundActors)
+	{
+		if (GetNumberOfInhabitants() >= MaxNumberOfCreatures) break;
+		
+		if (ACreatureBase* Creature = Cast<ACreatureBase>(Actor))
+		{
+			if (Creature->HasAHome()) continue;
+			
+			InhabitingCreatures.Add(Creature);
+			Creature->AssignToHouse(this);
+		}
+	}
+}
--- Source/Elderwild/Public/Buildings/House.h
+++ Source/Elderwild/Public/Buildings/House.h
@@ -9,5 +9,17 @@ UCLASS()
 class ELDERWILD_API AHouse : public ABuildingBase
 {
 	GENERATED_BODY()
-	
+
+public:	
+	AHouse();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void TryFillBuildingWithCreatures();
+
 };
