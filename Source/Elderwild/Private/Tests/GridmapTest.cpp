fdf8354897d5550c6d1b47a4acec60035dfd109c
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-05-12 18:14:46 +0200

    Initial Commit

--- Source/Elderwild/Private/Tests/GridmapTest.cpp
+++ Source/Elderwild/Private/Tests/GridmapTest.cpp
@@ -1,6 +1,25 @@
+#include "Gridmap/Grid.h"
+#include "Gridmap/GridDimensions.h"
+
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FGridmap, "Elderwild.Gridmap", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
 
 bool FGridmap::RunTest(const FString& Parameters)
 {
+	AGrid* Grid = NewObject<AGrid>();
+
+	TestNotNull(TEXT("Grid successfully created"), Grid);
+
+	// TODO : this is inconclusive
+	
+	//UGridDimensions* GetGridDimensions() {return GridDimensions;}
+	
+	//void HoverTile(FVector Location);
+
+	//void UnhoverTile();
+
+	//void TryBuild(FIntVector2 TileToBuildOn);
+	
+	//void ReceiveMouseMode(EMouseMode NewMouseMode);
+	
 	return true;
 }
