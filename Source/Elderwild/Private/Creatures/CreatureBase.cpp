ae185c89e4c62ddc5871ebc18030eae93adff5fc
Lewis Wardman <lewis2304@hotmail.co.uk>
2025-05-12 18:15:57 +0200

    Simple logic for sleeping

--- Source/Elderwild/Private/Creatures/CreatureBase.cpp
+++ Source/Elderwild/Private/Creatures/CreatureBase.cpp
@@ -1,5 +1,9 @@
 #include "Creatures/CreatureBase.h"
 
+#include "Kismet/GameplayStatics.h"
+
+#include "Calendar/Calendar.h"
+#include "GameModes/DevGameMode.h"
 
 ACreatureBase::ACreatureBase()
 {
@@ -14,9 +18,47 @@ void ACreatureBase::BeginPlay()
 void ACreatureBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (ShouldBeSleeping())
+	{
+		//UE_LOG(LogTemp, Display, TEXT("Should be sleeping"));
+	}
+	else
+	{
+		//UE_LOG(LogTemp, Display, TEXT("Should be working"));
+	}
 }
 
 void ACreatureBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ACreatureBase::AssignToHouse(AHouse* NewHouse)
+{
+	House = NewHouse;
+}
+
+void ACreatureBase::RemoveFromHouse()
+{
+	House = nullptr;
+}
+
+bool ACreatureBase::HasAHome()
+{
+	return House != nullptr;
+}
+
+bool ACreatureBase::ShouldBeSleeping()
+{
+	AGameModeBase* GM = UGameplayStatics::GetGameMode(this);
+
+	ADevGameMode* DevGameMode = Cast<ADevGameMode>(GM);
+
+	if (!DevGameMode) return false;
+	if (!DevGameMode->GetCalendar()) return false;
+	
+	EDayNight CurrentDayState = DevGameMode->GetCalendar()->GetDayState();
+
+	return SleepsDuringTime == CurrentDayState;
 }
--- Source/Elderwild/Public/Creatures/CreatureBase.h
+++ Source/Elderwild/Public/Creatures/CreatureBase.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "Calendar/DayCycler.h" // TODO : move EDayNight to its own file.
+
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "CreatureBase.generated.h"
 
-class ACreatureController;
+class AHouse;
+
+enum class EDayNight : uint8;
 
 UCLASS()
 class ELDERWILD_API ACreatureBase : public ACharacter
@@ -21,4 +25,20 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// TODO : Move to an interface?
+	void AssignToHouse(AHouse* NewHouse);
+
+	void RemoveFromHouse();
+
+	bool HasAHome();
+
+	EDayNight SleepsDuringTime = EDayNight::Day;
+
+	// TODO : make this respond to DayCycler events instead?
+	bool ShouldBeSleeping();
+	
+private:
+	UPROPERTY()
+	AHouse* House;
 };
